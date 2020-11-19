1e92cee5406ff7a817dd631a3d67cbbe63f65fd6
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2020-11-19 21:12:35 +0530

    added tailwind ui tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps /*, AppContext */ } from "next/app";
+import { Devtools } from "@ui-devtools/tailwind";
+
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Devtools>
+      <Component {...pageProps} />
+    </Devtools>
+  );
 }
 
 export default MyApp;
